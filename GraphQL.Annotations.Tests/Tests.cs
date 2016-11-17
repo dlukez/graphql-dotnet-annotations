4f5638411ace79c3da803cc1279b8e2fcc112f37
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2016-11-18 04:27:46 +1100

    Tests project stub

---
+++ GraphQL.Annotations.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace GraphQL.Annotations.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void GraphQLTypeAttribute_RefusesNonGraphTypes()
+        {
+
+            Assert.True(true);
+        }
+    }
+}
