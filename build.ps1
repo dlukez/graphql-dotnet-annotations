81cd7c63e2fe760ee710afa083bce39786378e10
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-01-31 13:27:31 +1100

    Fix build

--- build.ps1
+++ build.ps1
@@ -1,20 +1,19 @@
 param (
-  [Parameter(Mandatory=$true)]
+  [Parameter(Position = 1)]
   [string]$PrereleaseTag = $env:PrereleaseTag,
-  [Parameter()]
   [string]$Configuration = "Release"
 )
 
 dotnet restore
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet build src/GraphQL.Annotations/ --configuration $Configuration
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet test test/GraphQL.Annotations.Tests/ --configuration $Configuration
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet pack src/GraphQL.Annotations/ --configuration $Configuration --version-suffix $PrereleaseTag
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+if ($LASTEXITCODE -ne 0) { exit 1 }
 
 exit 0
