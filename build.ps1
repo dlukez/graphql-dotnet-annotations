800bf928df9692c8987277f6ab8b37361552f165
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-02-20 12:47:14 +1100

    Run GitVersion when building locally

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 # Setup
 $ErrorActionPreference = "Stop"
 if (-not $env:Configuration) { $env:Configuration = "Release" }
-if (-not $env:PackageVersion) { $env:PackageVersion = (gitversion | ConvertFrom-Json).FullSemVer }
+if (-not $env:BuildRunner) { $env:PackageVersion = (gitversion | ConvertFrom-Json).NuGetVersionV2 }
 
 # Helper functions
 function Invoke-BuildStep { param([scriptblock]$cmd) & $cmd; if ($LASTEXITCODE -ne 0) { exit 1 } }
