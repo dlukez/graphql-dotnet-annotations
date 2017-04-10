e4dd11071aba27468fe101ea250962e9ee8a386b
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-04-11 03:07:06 +1000

    Simplify build file

--- build.ps1
+++ build.ps1
@@ -7,8 +7,10 @@ if (-not $env:BuildRunner) { $env:PackageVersion = (gitversion | ConvertFrom-Jso
 function Invoke-BuildStep { param([scriptblock]$cmd) & $cmd; if ($LASTEXITCODE -ne 0) { exit 1 } }
 
 # Build
-Invoke-BuildStep { dotnet restore }
+Invoke-BuildStep { dotnet restore src/GraphQL.Annotations/GraphQL.Annotations.csproj }
 Invoke-BuildStep { dotnet pack src/GraphQL.Annotations/GraphQL.Annotations.csproj --include-symbols }
+
+Invoke-BuildStep { dotnet restore test/GraphQL.Annotations.Tests/GraphQL.Annotations.Tests.csproj }
 Invoke-BuildStep { dotnet test test/GraphQL.Annotations.Tests/GraphQL.Annotations.Tests.csproj }
 
 # End
